976cf5a2964c57ac1b580f4bf4559711293765d3
Fengping Hu <fengping@uchicago.edu>
2025-03-25 13:48:48 -0500

    fix

--- manager.py
+++ manager.py
@@ -278,7 +278,7 @@ class DaskClusterManager:
         #cluster.adapt(minimum=minimum, maximum=maximum)
 
         future = asyncio.run_coroutine_threadsafe(
-            cluster.gateway._adapt_cluster(cluster.name,minimum=minimum, maximum=maximum), self.loop.asyncio_loop
+            cluster.gateway._adapt_cluster(cluster.name,minimum=minimum, maximum=maximum), cluster.loop.asyncio_loop
         )
         try:
             print( future.result() )
