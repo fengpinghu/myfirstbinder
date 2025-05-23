ddad81cc1da4e9457227bd5c629578fb72c798be
Fengping Hu <fengping@uchicago.edu>
2025-05-22 19:35:39 -0500

    test

--- manager.py
+++ manager.py
@@ -159,9 +159,7 @@ class DaskClusterManager:
             self._adaptives[cluster_id] = adaptive
 
         print(f"{cluster_id}, {cluster_name} {cluster.name}")
-        name = cluster.name
-        print(f"{cluster_id}, {cluster_name} {name}")
-        save_tls_credentials(self.gateway.get_cluster(name))
+        save_tls_credentials( await self.gateway.get_cluster(cluster.name))
 
         self._clusters[cluster_id] = cluster
         self._cluster_names[cluster_id] = cluster_name
